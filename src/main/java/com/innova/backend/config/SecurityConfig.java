f7e57636723f697b4607bb45df4685c5a336766d
Zehra Uğurlu <zeugurlu@outlook.com>
2024-07-26 12:11:09 +0300

    Comment

--- src/main/java/com/innova/backend/config/SecurityConfig.java
+++ src/main/java/com/innova/backend/config/SecurityConfig.java
@@ -1,58 +1,58 @@
 package com.innova.backend.config;
 
-import io.swagger.v3.oas.annotations.info.License;
+//import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import lombok.Value;
+//import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsManager userDetailsManager (DataSource dataSource){
-        JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        theUserDetailsManager
-                .setUsersByUsernameQuery("SELECT username, password, true as enabled FROM users WHERE username = ?");
-
-
-        theUserDetailsManager
-                .setAuthoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
-        return theUserDetailsManager;
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(HttpMethod.GET,"api/**").hasAnyRole("USER")
-                        .requestMatchers(HttpMethod.POST,"api/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.DELETE,"api/**").hasAnyRole( "ADMIN")
-                        .requestMatchers(HttpMethod.PUT,"api/**").hasAnyRole( "ADMIN")
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
-    }
+//    @Bean
+//    public UserDetailsManager userDetailsManager (DataSource dataSource){
+//        JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        theUserDetailsManager
+//                .setUsersByUsernameQuery("SELECT username, password, true as enabled FROM users WHERE username = ?");
+//
+//
+//        theUserDetailsManager
+//                .setAuthoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
+//        return theUserDetailsManager;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers(HttpMethod.GET,"api/**").hasAnyRole("USER")
+//                        .requestMatchers(HttpMethod.POST,"api/**").hasAnyRole("USER", "ADMIN")
+//                        .requestMatchers(HttpMethod.DELETE,"api/**").hasAnyRole( "ADMIN")
+//                        .requestMatchers(HttpMethod.PUT,"api/**").hasAnyRole( "ADMIN")
+//                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//
+//        return http.build();
+//    }
 
 
 
--- src/main/java/com/innova/backend/controller/scheduledController/SpendingController.java
+++ src/main/java/com/innova/backend/controller/scheduledController/SpendingController.java
@@ -21,4 +21,6 @@ public class SpendingController {
         return totalSpending;
     }
 
+
+
 }
